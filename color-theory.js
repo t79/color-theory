cc6625822750014dac84b2893b3407c0e6e9860c
Terje Urnes <terje@urnes.net>
2022-02-10 11:39:31 +0100

    Adding the javascript file

---
+++ color-theory.js
@@ -0,0 +1,13 @@
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === 'interactive') {
+        initTheory();
+    }
+});
+
+t79T = {
+
+}
+
+function initTheory() {
+
+}

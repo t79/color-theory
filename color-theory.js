758d37cb0590079b43f646c0cf099a4f48880649
Terje Urnes <terje@urnes.net>
2022-02-10 21:35:37 +0100

    Fullscreen View: making Images ready
    Fullscreen View: making images ready

--- color-theory.js
+++ color-theory.js
@@ -1,13 +1,24 @@
 document.addEventListener('readystatechange', function () {
     if (document.readyState === 'interactive') {
-        initTheory();
+        collectAllFeatureImagesForFullscreenView();
     }
 });
 
-t79T = {
+t79FV = {
 
 }
 
-function initTheory() {
+function collectAllFeatureImagesForFullscreenView() {
+
+    const images = document.querySelectorAll('#main-content img');
+    const imageArray = Array.from(images);
+
+    for (imageIndex in imageArray) {
+        imageArray[imageIndex].id = 'image-id-' + imageIndex;
+        imageArray[imageIndex].setAttribute('onclick', 'goIntoFullscreenImageView(this)');
+        imageArray[imageIndex].getElementsByClassName.cursor = 'zoom-in';
+    }
+    t79FV.totalNumberOfImages = imageArray.length;
 
 }
+
